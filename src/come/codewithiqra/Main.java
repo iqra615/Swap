fcc65f9553802b31fbff8d9c57e0e678df6096c0
HP 6th <iqra615>
2020-06-18 08:15:49 -0700

    Swap

---
+++ src/come/codewithiqra/Main.java
@@ -0,0 +1,17 @@
+package come.codewithiqra;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int a = 20;
+            int b = 30;
+            int temp;
+
+            temp = a;
+            a = b;
+            b = temp;
+        System.out.println("a="+ a);
+        System.out.println("b="+ b);
+
+    }
+}
